d551bf211ff8690772823204f8eed840f37c9c4f
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-19 01:55:33 -0300

    Adicionado arquivos de cabeçario e constantes de cores

--- sdl2/sdl2.h
+++ sdl2/sdl2.h
@@ -1,6 +1,16 @@
 #ifndef SDL2_H_INCLUDED
 #define SDL2_H_INCLUDED
 
-//  Code
+//  Bibliotecas
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+/*  Constantes */
+#define BRANCO 255,255,255
+#define PRETO 0,0,0
+#define VERMELHO 255,0,0
+#define VERDE 0,255,0
+#define AZUL 0,0,255
 
 #endif // SDL2_H_INCLUDED
