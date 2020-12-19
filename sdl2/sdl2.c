e449ea6b532fa7d2cf7c980cc12c588d01c01b91
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-19 01:59:00 -0300

    Funções responsaveis pelo uso da janela SDL adicionada

--- sdl2/sdl2.c
+++ sdl2/sdl2.c
@@ -49,3 +49,60 @@ SDL_Window* sdl2_criar_janela(char nome[], int largura, int altura)
     }
     return janela;
 }
+
+/*  Funcoes da janela e tela */
+//  Funcao que cria uma janela
+SDL_Window* sdl2_criar_janela(char nome[], int largura, int altura)
+{
+    SDL_Window *janela = NULL;
+    janela = SDL_CreateWindow(nome, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, largura, altura, SDL_WINDOW_SHOWN);
+    if(janela == NULL)
+    {
+        printf("Erro ao criar janela: %s \n", SDL_GetError());
+        sdl2_finalizar_SDL2();
+        exit(1);
+    }
+    return janela;
+}
+
+//  Funcao que cria uma tela
+SDL_Renderer* sdl2_criar_tela(SDL_Window *janela)
+{
+    SDL_Renderer *tela = NULL;
+    tela = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(tela == NULL)
+    {
+        printf("Erro ao criar tela: %s \n", SDL_GetError());
+        sdl2_fechar_janela(janela);
+        sdl2_finalizar_SDL2();
+        exit(1);
+    }
+    return tela;
+}
+
+//  Funcao que define a cor de uma tela
+void sdl2_definir_cor_tela(SDL_Renderer *tela, int r, int g, int b)
+{
+    SDL_SetRenderDrawColor(tela, r, g, b, 255);
+    SDL_RenderClear(tela);
+}
+
+//  Funcao que atualiza uma tela
+void sdl2_atualizar_tela(SDL_Renderer *tela)
+{
+    SDL_RenderPresent(tela);
+}
+
+//  Funcao que libera memoria de uma janela
+void sdl2_fechar_janela(SDL_Window *janela)
+{
+    SDL_DestroyWindow(janela);
+    janela = NULL;
+}
+
+//  Funcao que libera memoria de uma tela
+void sdl2_fechar_tela(SDL_Renderer *tela)
+{
+    SDL_DestroyRenderer(tela);
+    tela = NULL;
+}
