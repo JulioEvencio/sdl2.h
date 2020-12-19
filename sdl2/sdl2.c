03fd6d26703ace471cd707cb238bea6a47c223c2
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-19 01:54:12 -0300

    Criado os arquivos

---
+++ sdl2/sdl2.c
@@ -0,0 +1,5 @@
+//  Bibliotecas
+#include <stdio.h>
+#include "sdl2/sdl2.h"
+
+/*  Funcoes */
---
+++ sdl2/sdl2.h
@@ -0,0 +1,6 @@
+#ifndef SDL2_H_INCLUDED
+#define SDL2_H_INCLUDED
+
+//  Code
+
+#endif // SDL2_H_INCLUDED
