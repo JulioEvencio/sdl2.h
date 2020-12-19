66f0c64c62c09e571df967fb7e3f58c51c8fa74b
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-19 02:06:48 -0300

    Correção de bugs

--- sdl2/sdl2.c
+++ sdl2/sdl2.c
@@ -1,6 +1,6 @@
 //  Bibliotecas
 #include <stdio.h>
-#include "sdl2/sdl2.h"
+#include "sdl2.h"
 
 /*  Funcoes */
 /*  Inicializacao e finalizacao: SDL, IMG e TTF */
@@ -33,21 +33,6 @@ void sdl2_finalizar_SDL2(void)
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
-}
-
-/*  Funcoes da janela e tela */
-//  Funcao que cria uma janela
-SDL_Window* sdl2_criar_janela(char nome[], int largura, int altura)
-{
-    SDL_Window *janela = NULL;
-    janela = SDL_CreateWindow(nome, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, largura, altura, SDL_WINDOW_SHOWN);
-    if(janela == NULL)
-    {
-        printf("Erro ao criar janela: %s \n", SDL_GetError());
-        sdl2_finalizar_SDL2();
-        exit(1);
-    }
-    return janela;
 }
 
 /*  Funcoes da janela e tela */
